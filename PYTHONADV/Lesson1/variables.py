59996aa5d3794e7a665283ddeac51280dd0cf698
Festee <fd61273@ubt-uni.net>
2026-09-12 15:02:54 +0200

    Add 19-30B and PYTHONADV

---
+++ PYTHONADV/Lesson1/variables.py
@@ -0,0 +1,56 @@
+# $emri = "Festim" -- ne php
+
+# emri_i_variables = vlera
+
+# temperature = 98.6
+#
+# name = "Bleard"
+#
+# print(type(temperature))
+# print(type(name))
+
+#float,string,int,boolean,array - data types
+
+# x=5
+# y=3
+# result = x * y
+# print(result)
+
+#krijone ni variable qe e run emrin, njo qe e run mbiemrin, njo qe run moshen edhe nfund mu shfaq
+#me ni fjali resultati
+
+# firstname="fest"
+# lastname=" dibrani "
+# age="23"
+#
+# result = firstname + lastname +age
+#
+# print(result)
+
+#integer - numra 1 , 100, -5
+#float - numra me presje(pike)  3.14, 2.26, -0.5
+#string - tekst
+#boolean - true ose false
+#list - collection of items [1,2,3] ["test","test","test"]
+#tuple - like list but cannot be changed after creation (1,2,3)
+#dictionary - key/value pairs  {"name":"fest","age":23}
+
+to_do_list = ["math_homework","research for biology","clean the room","play cs"]
+first_item = to_do_list[0]
+second_item = to_do_list[1]
+third_item = to_do_list[2]
+last_item = to_do_list[3]
+
+print(last_item)
+print(to_do_list[2])
+
+#.append - shton element
+#.clear - largon elementet
+#.count - me numru elemente
+#.copy - copy/paste te listes
+#.index - indeksin ku ndodhet elementi ne liste
+#.insert - me shtu element ne indeksin e caktum
+#.pop - largon elementin ne indexksin e caktum
+#.remove - largon elementin e caktum
+#.reverse - e kthen array reverse
+
